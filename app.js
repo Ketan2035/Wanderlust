aa80bc6b67f8b4643ad7be821050674cf02f150a
ketan2035 <ketankumar147856@gmail.com>
2025-08-30 02:01:58 +0530

    routing done

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
+const path=require("path");
 const listing=require("./models/listing")
+const methodOverride=require("method-override");
 
 main().then((res)=>{
     console.log("connection succesfull");
@@ -12,18 +14,81 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
-app.get("/testlisting",async(req,res)=>{
-    let samplelisting=new listing({
-        title:"my new villa",
-        description:"by the beach",
-        price:1200,
-        location:"vaishali ,bihar",
-        country:"india",
-    })
-    await samplelisting.save();
-    console.log("sample was saved");
-    res.send("testing sucessful")
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.static(path.join(__dirname ,"public")));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+//index route
+
+app.get("/listings",async(req,res)=>{
+    const alllistings=await listing.find({});
+    res.render("listings/index.ejs",{alllistings});
 })
+
+//new route
+
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+})
+
+//show route
+
+app.get("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    const Listing=await listing.findById(id); 
+    res.render("listings/show.ejs",{Listing});
+})
+
+//create route
+
+app.post("/listings",async(req,res)=>{
+    // let {title,description,image,price ,location,country}=req.body;
+    const newlisting=new listing(req.body.listing);
+    await newlisting.save();
+    res.redirect("/listings");
+})
+
+//edit   route
+
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    const Listing=await listing.findById(id); 
+    res.render("listings/edit.ejs",{Listing});
+})
+
+//update route
+
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//delete route
+
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    let deletelisting=await listing.findByIdAndDelete(id);
+    console.log(deletelisting);
+    res.redirect("/listings"); 
+})
+
+
+// app.get("/testlisting",async(req,res)=>{
+//     let samplelisting=new listing({
+//         title:"my new villa",
+//         description:"by the beach",
+//         price:1200,
+//         location:"vaishali ,bihar",
+//         country:"india",
+//     })
+//     await samplelisting.save();
+//     console.log("sample was saved");
+//     res.send("testing sucessful")
+// })
 
 app.get("/",(req,res)=>{
     res.send("hii this is root");
