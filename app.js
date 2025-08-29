c9e2b816579823fdd731662b05ad31d0825f099f
ketan2035 <ketankumar147856@gmail.com>
2025-08-29 20:01:19 +0530

    database is connected

---
+++ app.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+
+main().then((res)=>{
+    console.log("connection succesfull");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+}
+
+app.get("/",(req,res)=>{
+    res.send("hii this is root");
+})
+
+app.listen(8080,()=>{
+    console.log("server is listening on port 8080");
+})
