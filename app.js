1bab1d526eebf67af22724dec62277a1f96d5d88
ketan2035 <ketankumar147856@gmail.com>
2025-08-30 13:39:11 +0530

    navbar & footer is done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose=require("mongoose");
 const path=require("path");
 const listing=require("./models/listing")
 const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
 
 main().then((res)=>{
     console.log("connection succesfull");
@@ -20,6 +21,8 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname ,"public")));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 //index route
 
